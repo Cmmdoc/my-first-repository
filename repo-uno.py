c25fe94b8156c6c546598df2359b0265b772918d
Cmmdoc <mrmagne01@outlook.com>
2024-05-29 12:34:43 -0400

    uploadb

--- repo-uno.py
+++ repo-uno.py
@@ -5,3 +5,11 @@ savings = premium_jelly
 
 if savings >= 1000000:
     print("I'm rich!")
+
+positions_a = [4,6,9]
+positions_a.append(24)
+
+print(positions_a)
+
+positions_a.insert(2, 45)
+print(positions_a)
