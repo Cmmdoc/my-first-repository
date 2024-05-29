e0ebab47c857809c602c8e7a77bc388dd17f47bd
Cmmdoc <mrmagne01@outlook.com>
2024-05-29 12:52:00 -0400

    UploadD

--- repo-uno.py
+++ repo-uno.py
@@ -6,15 +6,16 @@ savings = premium_jelly
 if savings >= 1000000:
     print("I'm rich!")
 
+## Use .append to add elements to the end of a list
 positions_a = [4,6,9]
 positions_a.append(24)
-
 print(positions_a)
 
+## Use .insert to add values to any part of a list, with (index, element)
+# you can only add one value at a time!
 positions_a.insert(2, 45)
-
 print(positions_a)
 
+## You can delete values from a list using .pop(index) 
 positions_a.pop(3)
-
 print(positions_a)
