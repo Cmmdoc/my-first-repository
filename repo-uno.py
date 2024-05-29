13c9c5ca9933966f7a27dc7bc5b0052ca91ddeec
Cmmdoc <mrmagne01@outlook.com>
2024-05-29 12:56:04 -0400

    UploadE

--- repo-uno.py
+++ repo-uno.py
@@ -6,6 +6,11 @@ savings = premium_jelly
 if savings >= 1000000:
     print("I'm rich!")
 
+# We use square brackets to make lists []
+
+items_a = ["Guava", "Potato", "674"]
+print(items_a)
+
 ## Use .append to add elements to the end of a list
 positions_a = [4,6,9]
 positions_a.append(24)
